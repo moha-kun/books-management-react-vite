23a89bd84caee729ee141a0e4ead3e279aee0f31
moha-kun <naitmoussamohamed@gmail.com>
2024-04-07 17:16:22 +0000

    finalization

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BooksPage from "./pages/Book/BooksPage";
 import Header from "./components/Header/Header";
 import UsersPage from "./pages/User/UsersPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/books/*" element={<BooksPage />} />
         <Route path="/users/*" element={<UsersPage />} />
         <Route path="*" element={<h1>Page Not Found 404</h1>} />
